058226b67518e807b761bd4df2b8157165e4c8e6
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-10-15 14:19:56 +0100

    try to get netlify to use fs instead of read function

--- src/lib/data/index.js
+++ src/lib/data/index.js
@@ -1,7 +1,9 @@
 import { read } from "$app/server";
+import { readFileSync } from "fs";
+import { join } from "path";
 
 // Make Vite import the JSON files in this directory into the build
-export const files = import.meta.glob("$lib/data/*.json", {
+export const files = import.meta.glob("./*.json", {
   query: "?url",
   import: "default",
   eager: true
@@ -16,8 +18,14 @@ export default async function(key) {
 
   if (cache[key]) return cache[key];
 
-  const file = read(files[path]);
-  const data = await file.json();
+  let data;
+  if (process.env.IS_NETLIFY) {
+    const file = readFileSync(join(process.cwd(), "src/lib/data", path));
+    data = JSON.parse(file);
+  } else {
+    const file = read(files[path]);
+    data = await file.json();
+  }
   cache[key] = data;
 
   return data;
--- src/routes/(api)/api/isnetlify/+server.js
+++
@@ -1,5 +0,0 @@
-import { text } from "@sveltejs/kit";
-
-export async function GET() {
-  return text(process.env.IS_NETLIFY ? "This is Netlify" : "This is not Netlify");
-}
